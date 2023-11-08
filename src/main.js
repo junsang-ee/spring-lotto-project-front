5b5a0c3fa4b1e252a5aa151c20a53264c9bd77a6
DESKTOP-5GJCEAC\Administrator <dkwnstkdk@gmail.com>
2023-11-09 04:58:36 +0900

    Add package(module) router, axios, vue-router, vue Modify main.js axios baseUrl, and static Header Add server host and post In vite config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+import axios from 'axios';
+import router from './router/router';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "http://localhost:8080"
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+createApp(App)
+  .use(router)
+  .mount('#app')
