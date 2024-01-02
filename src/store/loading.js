953df4e6292258350dd46a05998dd14e82bafeb4
junsang-ee <dkwnstkddk@gmail.com>
2024-01-02 19:45:24 +0900

    Add package vuetify and pinia Create and Add loading.js file (in store package)

---
+++ src/store/loading.js
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+import { computed, ref, watch } from "vue";
+
+export const useLoadingStore = defineStore("loading", () => {
+
+    const loading = ref(false);
+  
+    const setLoading = (value) => {
+      loading.value = value;
+    }
+  
+    watch(loading, (l) => {
+      console.log(`Loading!!! ${ l }`)
+    })
+  
+    return { setLoading, isLoading: computed(() => loading.value) }
+  })
